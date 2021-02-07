3ab0ea0d0179d0fee3b08583721d8e83ae668437
ventisleftarm <dohaalyssa@gmail.com>
2021-02-07 10:12:32 +0000

    saving algorithm testing

--- src/timing/algotest.py
+++ src/timing/algotest.py
@@ -33,7 +33,7 @@ def sort_list(unsorted_list, **kwargs):
     return unsorted_list.sort()
 
 time_list = []
-length_list = [1e5,2e5,5e5,1e6,2e6,5e6]
+length_list = [1e5,2e5,3e5,5e5,1e6,2e6]
 for i in length_list:
     logtime_data ={}
     sort_list(return_random_list(int(i)), log_time=logtime_data)
@@ -42,4 +42,10 @@ for i in length_list:
 plt.plot(length_list, time_list, "x")
 plt.xlabel("length of list")
 plt.ylabel("time to sort (seconds)")
+plt.show()
+log_length_list = [math.log(x, 10) for x in length_list]
+log_time_list = [math.log(x, 10) for x in time_list]
+plt.plot(log_length_list, log_time_list, "x")
+plt.xlabel("$\log_{10}(L)$")
+plt.ylabel("$\log_{10}(T)$")
 plt.show()
